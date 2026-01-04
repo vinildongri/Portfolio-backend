13f673f7d59a375ca5367a5be8f94e20021d5098
vinildongri <dongrivinil@gmail.com>
2026-01-05 02:30:59 +0530

    connected to frontend

--- app.js
+++ app.js
@@ -7,7 +7,16 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+// app.use(cors({
+//     origin: "https://vinilportfolio.com" 
+// }));
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,  // your frontend URL
+    credentials: true,
+}));
+
+
 app.use(express.json());
 
 app.use("/api/v1", contactRoutes);
