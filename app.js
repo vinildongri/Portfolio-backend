5f17b5d8099924c54ef72e036428f08a8e099a34
vinildongri <dongrivinil@gmail.com>
2026-01-07 02:41:28 +0530

    last fix

--- app.js
+++ app.js
@@ -16,24 +16,6 @@ app.use(cors({
     credentials: true,
 }));
 
-// app.use(cors({
-//   origin: process.env.FRONTEND_URL,
-//   methods: [ "POST"],
-//   credentials: true
-// }));
-
-// app.use(
-//   cors({
-//     origin: process.env.FRONTEND_URL,
-//     methods: ["GET", "POST", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     credentials: true,
-//   })
-// );
-
-// app.options("*", cors());
-
-
 
 app.use(express.json());
 
@@ -44,43 +26,3 @@ app.listen(process.env.PORT, () => {
         `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode`
     );
 });
-
-
-
-
-
-
-
-
-// import express from "express";
-// import dotenv from "dotenv";
-// import cors from "cors";
-// import contactRoutes from "./routes/contact.js";
-
-// dotenv.config();
-
-// const app = express();
-
-// app.use(
-//   cors({
-//     origin: process.env.FRONTEND_URL,
-//     methods: ["POST", "GET", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     credentials: true,
-//   })
-// );
-
-// app.use(express.json());
-// app.use("/api/v1", contactRoutes);
-
-// /* ✅ LOCAL ONLY */
-// if (process.env.NODE_ENV !== "production") {
-//   app.listen(process.env.PORT, () => {
-//     console.log(
-//       `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode`
-//     );
-//   });
-// }
-
-// /* ✅ VERCEL */
-// export default app;
